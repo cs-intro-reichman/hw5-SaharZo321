75921bd69b5952b9285b3ecd8dad8f6e15e439ac
Sahar Zolberg <saharz321@gmail.com>
2024-12-05 21:36:27 +0200

    another fix to scrabble stupid line breakers

--- Scrabble.java
+++ Scrabble.java
@@ -3,9 +3,11 @@
  */
 public class Scrabble {
 
-	// Note 1: "Class variables", like the five class-level variables declared below,
+	// Note 1: "Class variables", like the five class-level variables declared
+	// below,
 	// are global variables that can be accessed by any function in the class. It is
-	// customary to name class variables using capital letters and underline characters.
+	// customary to name class variables using capital letters and underline
+	// characters.
 	// Note 2: If a variable is declared "final", it is treated as a constant value
 	// which is initialized once and cannot be changed later.
 
@@ -15,7 +17,7 @@ public class Scrabble {
 	// The "Scrabble value" of each letter in the English alphabet.
 	// 'a' is worth 1 point, 'b' is worth 3 points, ..., z is worth 10 points.
 	static final int[] SCRABBLE_LETTER_VALUES = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
-												  1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
+			1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
 
 	// Number of random letters dealt at each round of this Scrabble game
 	static int HAND_SIZE = 10;
@@ -23,27 +25,31 @@ public class Scrabble {
 	// Maximum number of possible words in this Scrabble game
 	static int MAX_NUMBER_OF_WORDS = 100000;
 
-    // The dictionary array (will contain the words from the dictionary file)
+	// The dictionary array (will contain the words from the dictionary file)
 	static String[] DICTIONARY = new String[MAX_NUMBER_OF_WORDS];
 
 	// Actual number of words in the dictionary (set by the init function, below)
 	static int NUM_OF_WORDS;
 
-	// Populates the DICTIONARY array with the lowercase version of all the words read
-	// from the WORDS_FILE, and sets NUM_OF_WORDS to the number of words read from the file.
+	// Populates the DICTIONARY array with the lowercase version of all the words
+	// read
+	// from the WORDS_FILE, and sets NUM_OF_WORDS to the number of words read from
+	// the file.
 	public static void init() {
-		// Declares the variable in to refer to an object of type In, and initializes it to represent
-		// the stream of characters coming from the given file. Used for reading words from the file.  
+		// Declares the variable in to refer to an object of type In, and initializes it
+		// to represent
+		// the stream of characters coming from the given file. Used for reading words
+		// from the file.
 		In in = new In(WORDS_FILE);
-        System.out.println("Loading word list from file...");
-        NUM_OF_WORDS = 0;
+		System.out.println("Loading word list from file...");
+		NUM_OF_WORDS = 0;
 		while (!in.isEmpty()) {
-			// Reads the next "token" from the file. A token is defined as a string of 
-			// non-whitespace characters. Whitespace is either space characters, or  
+			// Reads the next "token" from the file. A token is defined as a string of
+			// non-whitespace characters. Whitespace is either space characters, or
 			// end-of-line characters.
 			DICTIONARY[NUM_OF_WORDS++] = in.readString().toLowerCase();
 		}
-        System.out.println(NUM_OF_WORDS + " words loaded.");
+		System.out.println(NUM_OF_WORDS + " words loaded.");
 	}
 
 	// Checks if the given word is in the dictionary.
@@ -55,9 +61,10 @@ public class Scrabble {
 		}
 		return false;
 	}
-	
+
 	// Returns the Scrabble score of the given word.
-	// If the length of the word equals the length of the hand, adds 50 points to the score.
+	// If the length of the word equals the length of the hand, adds 50 points to
+	// the score.
 	// If the word includes the sequence "runi", adds 1000 points to the game.
 	public static int wordScore(String word) {
 		int score = 0;
@@ -65,7 +72,7 @@ public class Scrabble {
 			char c = word.charAt(i);
 			int index = c - 'a';
 			score += SCRABBLE_LETTER_VALUES[index] * word.length();
-		
+
 		}
 		if (word.length() == HAND_SIZE) {
 			score += 50;
@@ -85,11 +92,12 @@ public class Scrabble {
 		hand = MyString.insertRandomly('a', hand);
 		return hand;
 	}
-	
-    // Runs a single hand in a Scrabble game. Each time the user enters a valid word:
-    // 1. The letters in the word are removed from the hand, which becomes smaller.
-    // 2. The user gets the Scrabble points of the entered word.
-    // 3. The user is prompted to enter another word, or '.' to end the hand. 
+
+	// Runs a single hand in a Scrabble game. Each time the user enters a valid
+	// word:
+	// 1. The letters in the word are removed from the hand, which becomes smaller.
+	// 2. The user gets the Scrabble points of the entered word.
+	// 3. The user is prompted to enter another word, or '.' to end the hand.
 	public static void playHand(String hand) {
 		int score = 0;
 		// Declares the variable in to refer to an object of type In, and initializes it
@@ -126,16 +134,16 @@ public class Scrabble {
 		} else {
 			System.out.println("End of hand. Total score: " + score + " points");
 		}
-		System.out.println();
 	}
 
-	// Plays a Scrabble game. Prompts the user to enter 'n' for playing a new hand, or 'e'
+	// Plays a Scrabble game. Prompts the user to enter 'n' for playing a new hand,
+	// or 'e'
 	// to end the game. If the user enters any other input, writes an error message.
 	public static void playGame() {
 		// Initializes the dictionary
-    	init();
-		// The variable in is set to represent the stream of characters 
-		// coming from the keyboard. Used for getting the user's inputs.  
+		init();
+		// The variable in is set to represent the stream of characters
+		// coming from the keyboard. Used for getting the user's inputs.
 		In in = new In();
 
 		while (true) {
@@ -150,16 +158,18 @@ public class Scrabble {
 			} else {
 				System.out.println("Invalid command.");
 			}
+			System.out.println();
+
 		}
 	}
 
 	public static void main(String[] args) {
 		//// Uncomment the test you want to run
-		////testBuildingTheDictionary();  
-		////testScrabbleScore();    
-		////testCreateHands();  
-		////testPlayHands();
-		////playGame();
+		//// testBuildingTheDictionary();
+		//// testScrabbleScore();
+		//// testCreateHands();
+		//// testPlayHands();
+		//// playGame();
 	}
 
 	public static void testBuildingTheDictionary() {
@@ -170,23 +180,24 @@ public class Scrabble {
 		}
 		System.out.println(isWordInDictionary("mango"));
 	}
-	
+
 	public static void testScrabbleScore() {
-		System.out.println(wordScore("bee"));	
+		System.out.println(wordScore("bee"));
 		System.out.println(wordScore("babe"));
 		System.out.println(wordScore("friendship"));
 		System.out.println(wordScore("running"));
 	}
-	
+
 	public static void testCreateHands() {
 		System.out.println(createHand());
 		System.out.println(createHand());
 		System.out.println(createHand());
 	}
+
 	public static void testPlayHands() {
 		init();
-		//playHand("ocostrza");
-		//playHand("arbffip");
-		//playHand("aretiin");
+		// playHand("ocostrza");
+		// playHand("arbffip");
+		// playHand("aretiin");
 	}
 }
